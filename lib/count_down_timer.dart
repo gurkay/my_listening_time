7de6e1173e8fc0eb8462ffe2f638befce4338f69
Gürkay Başyiğit <g_basyigit@hotmail.com>
2021-10-15 20:14:02 +0300

    count_down_timer new codes

--- lib/count_down_timer.dart
+++ lib/count_down_timer.dart
@@ -1,1 +1,18 @@
+import 'dart:async';
+import './timer_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+class CountDownTimer {
+  double _radius = 1;
+  bool _isActive = true;
+  Timer timer;
+  Duration _time;
+  Duration _fullTime;
+  int work = 30;
+  int shortBreak = 5;
+  int longBreak = 20;
+  
+  String returnTime(Duration t) {
+  
+  }
+}
