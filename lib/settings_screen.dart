0ad71c19e947201a1e52856ad3286c3fc685f3ee
Gürkay Başyiğit <g_basyigit@hotmail.com>
2021-10-15 21:59:22 +0300

    new codes

--- lib/settings_screen.dart
+++ lib/settings_screen.dart
@@ -1,1 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+class SettingsScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Settings'),
+      ),
+      body: Settings(),
+    );
+  }
+}
+
+class Settings extends StatefulWidget {
+  @override
+  _SettingsState createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+  TextEditingController txtWork;
+  TextEditingController txtShort;
+  TextEditingController txtLong;
+  static const String WORKTIME = 'workTime';
+  static const String SHORTBREAK = 'shortBreak';
+  static const String LONGBREAK = 'longBreak';
+  int workTime;
+  int shortBreak;
+  int longBreak;
+  SharedPreferences prefs;
+  
+  @override
+  void initState(){
+    txtWork = TextEditingController();
+    txtShort = TextEditingController();
+    txtLong = TextEditingController();
+    readSettings();
+    super.initState();
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    TextStyle textStyle = TextStyle(fontsize: 24);
+    return Container(
+      child: GridView.count(
+        scrollDirection: Axis.vertical,
+        crossAxisCount: 3,
+        childAspectRatio: 3,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        children: <Widget>[
+          Text('Work', style: textStyle),
+          
+        ],
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+      ),
+    );
+  }
+}
